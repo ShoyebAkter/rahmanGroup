c63938f81bdfb887dd9f3c16737fc1ad845c3a4e
Shoyeb <heroreal5385@gmail.com>
2024-11-02 09:29:22 +0600

    update personal info changed

--- src/containers/profile/modal/EditInfo1.jsx
+++ src/containers/profile/modal/EditInfo1.jsx
@@ -1,13 +1,17 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import swal from 'sweetalert';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import swal from "sweetalert";
 
 function EditInfo1({ profileInit, setProfileInit, isOpen, setIsOpen }) {
-  const overlayClass = isOpen ? 'fixed inset-0 bg-gray-500 opacity-75 z-50' : 'hidden';
-  const modalClass = isOpen ? 'fixed inset-0 flex items-center justify-center z-50' : 'hidden';
+  const overlayClass = isOpen
+    ? "fixed inset-0 bg-gray-500 opacity-75 z-50"
+    : "hidden";
+  const modalClass = isOpen
+    ? "fixed inset-0 flex items-center justify-center z-50"
+    : "hidden";
   const formClass = "border-2 border-x-gray-500 p-1 rounded-md";
-  const [loading, setLoading] = useState(false)
-  const apiUrl = process.env.REACT_APP_API_URL
+  const [loading, setLoading] = useState(false);
+  const apiUrl = process.env.REACT_APP_API_URL;
 
   const [formData, setFormData] = useState({
     id: profileInit.id,
@@ -21,137 +25,230 @@ function EditInfo1({ profileInit, setProfileInit, isOpen, setIsOpen }) {
     email: profileInit.email,
     idNo: profileInit.idNo,
     agentId: profileInit.agentId,
-  })
+  });
 
   const updateProfile = () => {
-    setLoading(true)
-    axios.put(`${apiUrl}/employee/updatePersonalInfo`, formData, {
-      headers: {
-        loginToken: localStorage.getItem("loginToken")
-      }
-    }).then(result => {
-        setLoading(false)
-        console.log(result)
-        if(result.data.message){
-           
-            setProfileInit({...profileInit,
-                firstName: formData.firstName,
-                lastName: formData.lastName,
-                DOB: formData.DOB,
-                mobile: formData.mobile,
-                telephone: formData.telephone,
-                status: formData.status,
-                idNo: formData.idNo,
-                email: formData.email,
-                agentId: formData.agentId,
-            })
+    setLoading(true);
+    axios
+      .put(`${apiUrl}/employee/updatePersonalInfo`, formData, {
+        headers: {
+          loginToken: localStorage.getItem("loginToken"),
+        },
+      })
+      .then((result) => {
+        setLoading(false);
+        console.log(result);
+        if (result.data.message) {
+          setProfileInit({
+            ...profileInit,
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            DOB: formData.DOB,
+            mobile: formData.mobile,
+            telephone: formData.telephone,
+            status: formData.status,
+            idNo: formData.idNo,
+            email: formData.email,
+            agentId: formData.agentId,
+          });
 
-            swal({
-                icon:"success",
-                title:"Update",
-                text:"Info Updated Successfully!",
-                buttons:false,
-                timer: "2000"
-            })
-            
-            setIsOpen(false)
+          swal({
+            icon: "success",
+            title: "Update",
+            text: "Info Updated Successfully!",
+            buttons: false,
+            timer: "2000",
+          });
 
-        }else{
-          console.log(result)
-          
-            swal({
-                icon:"error",
-                title:"Oops!",
-                text:result.data.error,
-            })
+          setIsOpen(false);
+        } else {
+          console.log(result);
+
+          swal({
+            icon: "error",
+            title: "Oops!",
+            text: result.data.error,
+          });
         }
-     
-      
-    }).catch(err => {
-      setLoading(false)
-      console.log(err)
-      if (err.response) {
-        swal({
-          title: 'Error',
-          icon: 'error',
-          text: err.response.data.error,
-        });
-      } else {
-        console.error(err);
-      }
-    })
-  }
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+        if (err.response) {
+          swal({
+            title: "Error",
+            icon: "error",
+            text: err.response.data.error,
+          });
+        } else {
+          console.error(err);
+        }
+      });
+  };
 
   return (
-    <div className='text-black'>
-        
+    <div className="text-black">
       <div className={overlayClass} onClick={() => setIsOpen(false)}></div>
 
       <div className={modalClass}>
-       
-        <div className="bg-white rounded-lg overflow-hidden md:w-1/4 w-2/3">
-          <div className="p-4">
-            <button className="float-right text-gray-500" onClick={() => setIsOpen(false)}>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+        <div className="bg-white rounded-xl shadow-lg overflow-hidden md:w-1/3 w-11/12 mx-auto">
+          <div className="p-6 relative">
+            <button
+              className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 focus:outline-none"
+              onClick={() => setIsOpen(false)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
               </svg>
             </button>
 
-            <h2 className='text-lg font-semibold p-2'>Update Personal Info</h2>
-            <hr />
+            <h2 className="text-2xl font-semibold text-gray-800 text-center mb-6">
+              Update Personal Info
+            </h2>
+            <hr className="mb-6 border-gray-300" />
 
-            <div className='p-2 flex flex-col'>
-              <label htmlFor="">Identification No.</label>
-              <input type="text" value={formData.idNo} className={formClass + " mb-3"} required
-                onChange={e => setFormData({...formData, idNo:e.target.value})} 
-              />
-              <label htmlFor="">First Name</label>
-              <input type="text" value={formData.firstName} className={formClass} required
-                onChange={e => setFormData({...formData, firstName:e.target.value})} 
-              />
-              <label htmlFor="" className='mt-4'>Last Name</label>
-              <input type="text" value={formData.lastName} className={formClass} required
-                onChange={e => setFormData({...formData, lastName:e.target.value})} 
-              />
-
-              <label htmlFor="" className='mt-4'>DOB</label>
-              <input type="text" value={formData.DOB} className={formClass} required
-                onChange={e => setFormData({...formData, DOB:e.target.value})} 
-              />
-
-              <label htmlFor="" className='mt-4'>Email</label>
-              <input type="text" value={formData.email} className={formClass} required
-                onChange={e => setFormData({...formData, email:e.target.value})} 
-              />
-
-              <label htmlFor="" className='mt-4'>Mobile</label>
-              <input type="text" value={formData.mobile} className={formClass} required
-                onChange={e => setFormData({...formData, mobile:e.target.value})} 
-              />
-
-              <label htmlFor="" className='mt-4'>Telephone</label>
-              <input 
+            <div className="space-y-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Identification No.
+              </label>
+              <input
                 type="text"
-                value={formData.telephone} 
-                className={formClass}
-                onChange={e => setFormData({...formData, telephone:e.target.value})}
+                value={formData.idNo}
+                onChange={(e) =>
+                  setFormData({ ...formData, idNo: e.target.value })
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
               />
 
+              <label className="block text-sm font-medium text-gray-700">
+                First Name
+              </label>
+              <input
+                type="text"
+                value={formData.firstName}
+                onChange={(e) =>
+                  setFormData({ ...formData, firstName: e.target.value })
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
 
-              <label htmlFor="" className='mt-4'>Status</label>
-              <select value={formData.status} className={formClass} required onChange={e => setFormData({...formData, status: e.target.value})}>
+              <label className="block text-sm font-medium text-gray-700">
+                Last Name
+              </label>
+              <input
+                type="text"
+                value={formData.lastName}
+                onChange={(e) =>
+                  setFormData({ ...formData, lastName: e.target.value })
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+
+              <label className="block text-sm font-medium text-gray-700">
+                DOB
+              </label>
+              <input
+                type="text"
+                value={formData.DOB}
+                onChange={(e) =>
+                  setFormData({ ...formData, DOB: e.target.value })
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+
+              <label className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+
+              <label className="block text-sm font-medium text-gray-700">
+                Mobile
+              </label>
+              <input
+                type="text"
+                value={formData.mobile}
+                onChange={(e) =>
+                  setFormData({ ...formData, mobile: e.target.value })
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+
+              <label className="block text-sm font-medium text-gray-700">
+                Telephone
+              </label>
+              <input
+                type="text"
+                value={formData.telephone}
+                onChange={(e) =>
+                  setFormData({ ...formData, telephone: e.target.value })
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+
+              <label className="block text-sm font-medium text-gray-700">
+                Status
+              </label>
+              <select
+                value={formData.status}
+                onChange={(e) =>
+                  setFormData({ ...formData, status: e.target.value })
+                }
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              >
                 <option value="Active">Active</option>
                 <option value="Inactive">Inactive</option>
               </select>
 
-
-              <button 
-                className='w-full mt-3 border-2 p-2 bg-green-600 text-white rounded-lg'
+              <button
                 onClick={() => updateProfile()}
-              >{loading ? "Updating ...": "Update"}</button>
-
+                className="w-full mt-4 py-2 bg-green-600 hover:bg-green-700 text-white font-semibold rounded-lg transition duration-150 ease-in-out flex items-center justify-center"
+                disabled={loading}
+              >
+                {loading ? (
+                  <svg
+                    className="animate-spin h-5 w-5 mr-3 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path d="M4 12a8 8 0 018-8v8H4z" fill="currentColor"></path>
+                  </svg>
+                ) : null}
+                {loading ? "Updating..." : "Update"}
+              </button>
             </div>
-            
           </div>
         </div>
       </div>
